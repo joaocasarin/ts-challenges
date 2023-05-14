99b7e5c4cd8078fc88c0f0b270b53e2673790b3a
Joao Vitor Casarin <jcasarin@apple.com>
2023-05-13 22:42:22 -0300

    fix: correct solution for leetcode 26

--- __tests__/remove-duplicates-from-sorted-array-26.test.ts
+++ __tests__/remove-duplicates-from-sorted-array-26.test.ts
@@ -1,13 +1,29 @@
 import removeDuplicates from '@src/remove-duplicates-from-sorted-array-26';
 
 describe('Remove duplicates from sorted array', () => {
-    test('should return 1', () => {
+    test('should return 2', () => {
         const nums = [1, 1, 2];
-        expect(removeDuplicates(nums)).toEqual(1);
+        const expectedNums = [1, 2];
+
+        const k = removeDuplicates(nums);
+
+        expect(expectedNums.length).toEqual(k);
+
+        for (let i = 0; i < k; i++) {
+            expect(nums[i]).toEqual(expectedNums[i]);
+        }
     });
 
     test('should return 5', () => {
         const nums = [0, 0, 1, 1, 1, 2, 2, 3, 3, 4];
-        expect(removeDuplicates(nums)).toEqual(5);
+        const expectedNums = [0, 1, 2, 3, 4];
+
+        const k = removeDuplicates(nums);
+
+        expect(expectedNums.length).toEqual(k);
+
+        for (let i = 0; i < k; i++) {
+            expect(nums[i]).toEqual(expectedNums[i]);
+        }
     });
 });
--- src/remove-duplicates-from-sorted-array-26.ts
+++ src/remove-duplicates-from-sorted-array-26.ts
@@ -1,14 +1,13 @@
 export default function removeDuplicates(nums: number[]): number {
-    const mapping = new Map<number, number>();
-    let count = 0;
+    if (!nums.length || nums.length === 1) return 0;
 
-    for (let i = 0; i < nums.length; i++) {
-        const num = nums[i];
-        mapping.set(num, (mapping.get(num) || 0) + 1);
-    }
+    let count = 1;
 
-    for (const [, v] of mapping) {
-        count += v - 1;
+    for (let i = 1; i < nums.length; i++) {
+        if (nums[i] !== nums[i - 1]) {
+            nums[count] = nums[i];
+            count++;
+        }
     }
 
     return count;
